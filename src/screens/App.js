7185fda53835aa46cccbc041c521a20c96bd6de7
wesenbergg <b.b.j.1998@gmail.com>
2020-09-29 22:42:44 +0300

    -m

---
+++ src/screens/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import TitleBar from 'frameless-titlebar';
+import icon from '../icon.png';
+import Main from './Main';
+import { ipcMock } from '../utils/ipcRendererMock';
+import { headerTheme } from '../utils/titleBarStyle';
+
+const { ipcRenderer } = window.require ? window.require('electron'): ipcMock;
+
+const App = () => {
+  
+  return(
+    <>
+    <TitleBar
+      icon={<img src={icon} className="icon" alt="icon" />}
+      app="Electron"
+      title="F stands for..."
+      theme={headerTheme}
+      className="titleBar"
+      onClose={() => ipcRenderer.send("close")}
+      onMinimize={() => ipcRenderer.send("minimize")}
+      onMaximize={() => ipcRenderer.send("maximize")}
+      //onDoubleClick={() => { enqueueSnackbar('double clicked', { variant: 'success' }) }}
+    />
+    <Main />
+    </>
+  );
+}
+
+export default App;
